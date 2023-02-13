ea9fca0790badfbee6a1f824900712acdd1a2031
Ubuntu <azureuser@rewardFreeLearning.4gfa352fyuwu3lrouppu4pzq3g.bx.internal.cloudapp.net>
2023-02-13 07:14:29 +0000

    pipeline

---
+++ data_collection.py
@@ -0,0 +1,51 @@
+import gym
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from casadi import *
+from casadi.tools import *
+import pdb
+import sys
+import do_mpc
+import d3rlpy
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import time
+
+from examples.batch_reactor.template_model import template_model
+from examples.batch_reactor.template_mpc import template_mpc
+from examples.batch_reactor.template_simulator import template_simulator
+
+from gym_env_demo import get_env
+from mpc_policy import get_mpc_controller
+
+dir_loc = os.path.dirname(os.path.relpath(__file__))
+
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--amlt', action='store_true', help="remote execution on amlt")
+parser.add_argument("--steps", type=int, help='total samples', default="1000")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    data_loc = os.environ['AMLT_DATA_DIR'] if args.amlt else dir_loc
+    dataset_dir = os.path.join(data_loc, 'datasets')
+
+    env = get_env()
+    policy = get_mpc_controller(env)
+    
+    num_steps = args.steps
+    # prepare experience replay buffer
+    buffer = d3rlpy.online.buffers.ReplayBuffer(maxlen=num_steps, env=env)
+
+    # start data collection
+    policy.collect(env, buffer, n_steps=num_steps)
+
+    # export as MDPDataset
+    dataset = buffer.to_mdp_dataset()
+
+    # save MDPDataset
+    os.makedirs(dataset_dir, exist_ok=True)
+    dataset.dump(f"{dataset_dir}/mpc_policy_batch_reactor_dataset_{num_steps}.h5")
--- mpc_policy.py
+++ mpc_policy.py
@@ -1,9 +1,10 @@
 from typing import Any, List, Sequence, Tuple, Union
-
+import sys
 import numpy as np
 from d3rlpy.argument_utility import ActionScalerArg
 from d3rlpy.constants import ActionSpace
 from d3rlpy.algos.base import AlgoBase
+from examples.batch_reactor.template_mpc import template_mpc
 
 
 class MPCPolicy(AlgoBase):
@@ -12,10 +13,10 @@ class MPCPolicy(AlgoBase):
     def __init__(
         self,
         *,
-        mpc_model,
-        noise,
-        min_actions,
-        max_actions,
+        mpc_model=None,
+        noise=None,
+        min_actions=None,
+        max_actions=None,
         action_scaler: ActionScalerArg = None,
         **kwargs: Any,
     ):
@@ -44,12 +45,23 @@ class MPCPolicy(AlgoBase):
         return self.sample_action(x)
 
     def sample_action(self, x: Union[np.ndarray, List[Any]]) -> np.ndarray:
-        x = np.asarray(x)
-        action = self.mpc_model.make_step(x)
-        if self.noise > 0:
-            random_action = [np.random.random()*(a_max-a_min) + a_min for a_min, a_max in zip(self.min_actions, self.max_actions)]
-            action = np.array([self.noise*r_a+(1.0-self.noise)*a for r_a, a in zip(random_action, action)])
-        return action
+        # x = np.asarray(x).flatten()
+        # Create the in-memory "file"
+        action_list = []
+        for obs in x:
+            try:
+                save_stdout = sys.stdout
+                sys.stdout = open('.trash.log', 'w')
+                ori_action = self.mpc_model.make_step(obs)
+                action = ori_action.flatten()
+            finally:
+                sys.stdout = save_stdout
+            
+            if self.noise > 0:
+                random_action = [np.random.random()*(a_max-a_min) + a_min for a_min, a_max in zip(self.min_actions, self.max_actions)]
+                action = np.array([self.noise*r_a+(1.0-self.noise)*a for r_a, a in zip(random_action, action)])
+            action_list.append(action)
+        return np.asarray(action_list)
 
     def predict_value(
         self,
@@ -61,3 +73,16 @@ class MPCPolicy(AlgoBase):
 
     def get_action_type(self) -> ActionSpace:
         return ActionSpace.CONTINUOUS
+    
+    
+    
+def get_mpc_controller(env):
+    mpc = template_mpc(env.model)
+    mpc.x0 = env.state
+    mpc.set_initial_guess()
+    
+    policy = MPCPolicy(mpc_model=mpc, 
+                       noise=0.1, 
+                       min_actions=env.min_actions, 
+                       max_actions=env.max_actions)
+    return policy
